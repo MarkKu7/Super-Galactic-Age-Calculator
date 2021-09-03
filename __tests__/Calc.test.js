e9ae263ae3c7f4c902f4de59b9b470968aafd787
MarkKu7 <marksrc7@gmail.com>
2021-09-03 11:59:20 -0700

    test 1 good fail

--- __tests__/Calc.test.js
+++ __tests__/Calc.test.js
@@ -1,9 +1,18 @@
-import ageCalculator from '../src/js/calc.js';
+import NewAge from '../src/js/Calc.js';
 
-describe ('newAge', () => {
+describe('NewAge', () => {
+
   let newAge;
 
-beforeEach(() => {
-  newAge = new ageCalculator(); 
+  beforeEach(() => {
+    newAge = new NewAge(10, 42, 16, 5, 1)
+  });
+
+test('should create a GalacticAge object', () => {
+    expect(newAge.currentAge).toEqual(10);
+    expect(newAge.mercuryAge).toEqual(42);
+    expect(newAge.venusAge).toEqual(16);
+    expect(newAge.marsAge).toEqual(5);
+    expect(newAge.jupiterAge).toEqual(1);
   });
 });
