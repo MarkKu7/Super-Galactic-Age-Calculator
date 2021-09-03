b2dfc4832f4570883c91bf6f8f7f945e1a5cc882
MarkKu7 <marksrc7@gmail.com>
2021-09-03 15:14:29 -0700

    pass on all tests

--- src/js/Calc.js
+++ src/js/Calc.js
@@ -19,12 +19,18 @@ export default class NewAge {
       return mercuryAge;
   }
   venusDays() {
-   
+    let days = this.currentAge * 365;
+    let venusAge = Math.round(days/226)
+    return venusAge;
   }
   marsDays() {
-    
+    let days = this.currentAge * 365;
+    let marsAge = Math.round(days/686)
+    return marsAge;
   }
   jupiterDays() {
-    
+    let days = this.currentAge * 365;
+    let jupiterAge = Math.round(days/4329)
+    return jupiterAge;
   }
 }
