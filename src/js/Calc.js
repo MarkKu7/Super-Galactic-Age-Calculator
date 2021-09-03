fb3575c06d4e9542154bd042de7e66dea2b74299
MarkKu7 <marksrc7@gmail.com>
2021-09-03 12:00:18 -0700

    test 1 good pass

--- src/js/Calc.js
+++ src/js/Calc.js
@@ -1,5 +1,10 @@
   
 export default class NewAge {
-  constructor() {
+  constructor(currentAge, mercuryAge, venusAge, marsAge, jupiterAge) {
+    this.currentAge = currentAge;
+    this.mercuryAge = mercuryAge;
+    this.venusAge = venusAge;
+    this.marsAge = marsAge;
+    this.jupiterAge = jupiterAge;
   }
 }
